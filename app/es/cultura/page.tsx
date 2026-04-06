ca97319d3d1b990e1bc74e2c4392de895ff6e23b
Alain Zulaika <registros@niala.es>
2026-04-06 17:25:55 +0200

    tablet: cultura hero line set to 235px

--- app/es/cultura/page.tsx
+++ app/es/cultura/page.tsx
@@ -138,7 +138,7 @@ export default function CulturaEuskeraPage() {
       {/* HERO */}
       <section className="mx-auto flex min-h-screen max-w-[1400px] flex-col justify-center px-8 md:px-16">
         <div className="relative -translate-y-8 md:-translate-y-10">
-          <div className="absolute left-0 top-0 w-[2px] bg-white/15 h-[222px] md:h-[240px] xl:h-[360px]" />
+          <div className="absolute left-0 top-0 w-[2px] bg-white/15 h-[222px] md:h-[235px] xl:h-[360px]" />
           <div className="pl-5 md:pl-10">
             <p className="hero-fade-1 mb-8 text-[0.82rem] tracking-[0.35em] text-[#2ED3E6]">
               <span className="uppercase">Cultura</span>
